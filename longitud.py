08a93fd2527eedff751e59805e53e8e23794d021
johans guzman <johansguzman08@gmail.com>
2023-09-04 09:11:34 -0300

    subiendo archivo johans guzman 3*d

---
+++ longitud.py
@@ -0,0 +1,10 @@
+def metros_a_pulgadas(metros):
+    return metros * 39.37
+
+def pulgadas_a_pies(pulgadas):
+    return pulgadas / 12
+
+if __name__ =="__main__":
+    metros=int(input("ingrese la cantidad de metros:  "))
+    pulgadas = metros_a_pulgadas(metros)
+    print(f"{metros} metros equivale a {pulgadas:.2f} pulgadas")
